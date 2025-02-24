f124c39712ecfc8fdd5112bb75a091af22ea924a
kevinsuoc <kseligmann@uoc.edu>
2025-02-24 10:01:43 +0100

    Delete file

--- usuario.sql
+++
@@ -1,4 +0,0 @@
-DROP USER IF EXISTS 'dataroast'@'%';
-CREATE USER 'dataroast'@'%' IDENTIFIED BY 'dataroastpassword';
-GRANT ALL PRIVILEGES ON senderosymontanas.* TO 'dataroast'@'%';
-FLUSH PRIVILEGES;
